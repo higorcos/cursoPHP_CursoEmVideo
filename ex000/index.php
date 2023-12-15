ed28f4de2a228ec497269648d23d423cc9f7ee9f
Hígor Pinheiro Costa <higorcosta2001@outlook.com>
2023-12-15 12:31:11 -0300

    init

---
+++ ex000/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello Word</title>
+</head>
+<body>
+    <h1>Hello Word PHP
+    </h1>
+
+        <?php
+            echo 'Olá, mundo';
+            // print 'Olá, mundo';
+        ?>
+    <p>Hora do recomeço !!</p>
+</body>
+</html>
