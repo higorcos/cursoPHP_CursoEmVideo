42782ade201fde95dd553c83625273c40aab756f
Hígor Pinheiro Costa <higorcosta2001@outlook.com>
2024-02-21 16:49:47 -0300

    routes

--- laravel_init/routes/web.php
+++ laravel_init/routes/web.php
@@ -16,3 +16,46 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+//Refatorando 
+Route::view('/', 'welcome');
+
+//parametro
+Route::get("/a/{id}", function ($id) {
+    return "" . $id;
+});
+//parametro opcional
+Route::get("/a/{id}/{id2?}", function ($id, $id2) {
+    return "" . $id . "  " . $id2;
+});
+
+//redirecinar
+Route::redirect("/redi", "/");
+
+//rotas nomeadas
+Route::get("/news", function () {
+    return "ROTA NOTICAS - NOMEADA";
+})->name("todas as noticias");
+
+Route::get("/novidades", function () {
+    return redirect()->route('todas as noticias');
+})->name("ultimas novidades");
+
+Route::get("/pagina-dos-admins", function () {
+    return redirect()->route('admin.home');
+})->name("redirect pagina admin");
+
+
+//Grupo de rotas
+//Admin
+Route::group([
+    'prefix' => 'admin',
+    'as' => 'admin.'
+], function () {
+
+    Route::get("home", function () {
+        return "ROTA ADMIN - Página principal";
+    })->name("home");
+    Route::get("list-users", function () {
+        return "ROTA ADMIN - lista de usuários";
+    })->name("users");
+});
